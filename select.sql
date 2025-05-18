5397be247fb1ae1ae01cc63f41ae5f284ed18419
showrov4g <showrovg00@gmail.com>
2025-05-18 12:55:16 +0600

    learning postgrace

---
+++ select.sql
@@ -0,0 +1,12 @@
+CREATE Table student2 (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    age INT,
+    grade CHAR(2),
+    course VARCHAR(50),
+    email VARCHAR(100),
+    dob DATE,
+    blood_group VARCHAR(5),
+    country VARCHAR(50)
+)
