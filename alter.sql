a0151a69918af972303b64415576570c90b4851c
showrov4g <showrovg00@gmail.com>
2025-05-18 12:04:22 +0600

    learning Sql

--- alter.sql
+++ alter.sql
@@ -12,4 +12,19 @@ ALTER Table person2
 
 ALTER Table person2
     RENAME COLUMN user_name to name
-    
+
+ALTER TABLE person2
+    alter COLUMN name type VARCHAR(50)
+
+
+
+Alter Table person2
+    alter COLUMN age set not NULL
+
+
+Alter Table person2
+    alter COLUMN age DROP not NULL
+
+
+alter Table person2
+    ADD constraint unique_person2_age UNIQUE(age)
