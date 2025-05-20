d4000246ed8ce861c291cffd7e4c11c16fd89516
showrov4g <showrovg00@gmail.com>
2025-05-20 20:30:16 +0600

    postgre learning

--- date.sql
+++ date.sql
@@ -5,6 +5,17 @@ select now()
 
 CREATE Table timez (ts TIMESTAMP Without time zone , tsz TIMESTAMP with time zone );
 
-insert into timez VALUES('2023-04-12');
+insert into timez VALUES('2023-04-12 10:45', '2023-04-12 10:45');
 
-SELECT *FROM timez
+SELECT *FROM timez;
+
+select CURRENT_DATE;
+
+SELECT now() :: date;
+
+
+SELECT to_char(now(), 'yyy/mm/dd');
+
+SELECT CURRENT_DATE - INTERVAL '2 year';
+
+SELECT *, age(CURRENT_DATE, dob) FROM student2
